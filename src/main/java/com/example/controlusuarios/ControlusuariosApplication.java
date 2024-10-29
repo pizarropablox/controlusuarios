bea16d990d816faea02efd8292fbf690afb9ceea
pizarropablox <32684457+pizarropablox@users.noreply.github.com>
2024-10-28 21:38:19 -0300

    primer commit

---
+++ src/main/java/com/example/controlusuarios/ControlusuariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlusuariosApplication.class, args);
+	}
+
+}
